9537d9363b9403ea229124a7787b7a8a1aeaf94e
Priscillia <priscilialauress@gmail.com>
2026-07-24 12:05:30 +0200

    Show database error

--- test-env.php
+++ test-env.php
@@ -4,3 +4,4 @@ echo "MYSQLHOST = " . getenv('MYSQLHOST') . "<br>";
 echo "MYSQLPORT = " . getenv('MYSQLPORT') . "<br>";
 echo "MYSQLDATABASE = " . getenv('MYSQLDATABASE') . "<br>";
 echo "MYSQLUSER = " . getenv('MYSQLUSER') . "<br>";
+echo "MYSQLPASSWORD = " . getenv('MYSQLPASSWORD') . "<br>";
